a61c21f67aceab86d25234138ae2e282512c4950
Danilo Figueiredo <grifotv@googlemail.com>
2020-01-24 11:21:31 +0000

    Update comment

--- android/src/main/java/com/henninghall/date_picker/Settings.java
+++ android/src/main/java/com/henninghall/date_picker/Settings.java
@@ -6,8 +6,7 @@ import android.text.format.DateFormat;
 public class Settings {
 
     public static boolean usesAmPm (){
-        // Fix for both:
-        // - https://github.com/henninghall/react-native-date-picker/issues/111#issuecomment-562577758
+        // Fix for:
         // - https://github.com/henninghall/react-native-date-picker/issues/130#issuecomment-577340060
     	return false;
         // return !DateFormat.is24HourFormat(DatePickerManager.context);
