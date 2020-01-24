dc23ec917b85f9aa9d8f4076750dce267f6421c2
Danilo Figueiredo <grifotv@googlemail.com>
2020-01-24 10:48:50 +0000

    Always use 24h format in Android

--- android/src/main/java/com/henninghall/date_picker/Settings.java
+++ android/src/main/java/com/henninghall/date_picker/Settings.java
@@ -6,7 +6,11 @@ import android.text.format.DateFormat;
 public class Settings {
 
     public static boolean usesAmPm (){
-        return !DateFormat.is24HourFormat(DatePickerManager.context);
+        // Fix for both:
+        // - https://github.com/henninghall/react-native-date-picker/issues/111#issuecomment-562577758
+        // - https://github.com/henninghall/react-native-date-picker/issues/130#issuecomment-577340060
+    	return false;
+        // return !DateFormat.is24HourFormat(DatePickerManager.context);
     }
 
 }
